1e9198bcc8487e71b5476ac9a1b9f786a753dcc9
Michelle <michelly-gomez@hotmail.com>
2021-01-28 09:48:47 -0500

    Configutacion de variables en quarkus y proyecto base

---
+++ src/test/java/org/curso/NativegettingIT.java
@@ -0,0 +1,9 @@
+package org.curso;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativegettingIT extends gettingTest {
+
+    // Execute the same tests but in native mode.
+}
