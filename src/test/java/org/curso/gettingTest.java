26a6134c816acea1740cd93e1c0546b875d6cc16
Michelle <michelly-gomez@hotmail.com>
2021-01-28 10:00:45 -0500

    Perfiles de configuracion

--- src/test/java/org/curso/gettingTest.java
+++ src/test/java/org/curso/gettingTest.java
@@ -15,7 +15,7 @@ public class gettingTest {
           .when().get("/prueba")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(is("Hola Mundo"));
     }
 
 }
